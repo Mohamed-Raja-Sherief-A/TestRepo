2741093989a5796f264db6cd772bfc493de07e22
Mohamed Raja <mohamedraja.329@gmail.com>
2023-01-21 20:55:28 +0530

    Main Calculator Display

--- Calculator/src/com/main/MainHandler.java
+++ Calculator/src/com/main/MainHandler.java
@@ -1,7 +1,23 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class MainHandler {
 public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	int ch=0;
+	do {
 	System.out.println("My Calculator App");
+	System.out.println();
+	System.out.println("1.Add");
+	System.out.println("2.Substract");
+	System.out.println("3.Multiply");
+	System.out.println("4.Division");
+	System.out.println("5.Exit");
+	ch = sc.nextInt();
+	
+	}while(ch!=5);
+	
 }
 }
