1e9828f62eaaed4b7d01abea186a58e45820257f
Mohamed Raja <mohamedraja.329@gmail.com>
2023-01-21 20:39:32 +0530

    Project initialised Calculator

---
+++ Calculator/src/com/main/MainHandler.java
@@ -0,0 +1,7 @@
+package com.main;
+
+public class MainHandler {
+public static void main(String[] args) {
+	System.out.println("My Calculator App");
+}
+}
---
+++ Calculator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mohamedrajasheriefa
+ *
+ */
+module Calculator {
+}
